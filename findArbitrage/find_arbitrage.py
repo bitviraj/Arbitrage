5c62955630aa8779e493512e14484a43e7dd6788
Garima singh <garima.miet@gmail.com>
2021-12-11 18:07:49 +0530

    added the database

--- findArbitrage/find_arbitrage.py
+++ findArbitrage/find_arbitrage.py
@@ -1,9 +1,23 @@
 import datetime
 import logging
 from .. import arbitrage_main
-
+import pyodbc
 import azure.functions as func
 
 
 def main(mytimer: func.TimerRequest) -> None:
-    best_case_token = arbitrage_main.get_arbitrage()
+    server = "LAPTOP-03Q3FH50"
+    database = "arbitrage"
+    cnxn = pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+        + server
+        + ";DATABASE="
+        + database
+        + ";Trusted_Connection=yes;"
+    )
+    cursor = cnxn.cursor()
+
+    result = arbitrage_main.get_arbitrage()[0]
+    my_token = result["symbol"]
+    my_return = (float(result["amount"]) - 1) * 100
+    my_time_stamp = datetime.datetime.now()
